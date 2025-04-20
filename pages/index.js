7f4f9448e07d8628b9e3fdc74a41e4f19e3c8fba
siestadivina <127675147+siestadivina@users.noreply.github.com>
2025-04-20 12:04:09 +0200

    Desde 0 con todos los pasos estructurados

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div style={{ padding: 20 }}>
-        <h1>🚀 Bienvenido a MovingWallet</h1>
-        <p>Todo está listo para empezar el desarrollo.</p>
-      </div>
-    );
-  }
-  
